680b1f9ae7d4572078d2f6fe321bcc30d3c6feba
Francisco Rubial <franjorub@gmail.com>
2021-06-03 12:59:54 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,55 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import Layout from "../components/layout";
+//import ArticlesComponent from "../components/articles";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+const IndexPage = () => {
+  const data = useStaticQuery(query);
+  
+  return (
+    <Layout seo={data.strapiHomepage.seo}>
+      <h1>{data.strapiHomepage.hero.title}</h1>
+      {data.allStrapiArticle 
+        ? <div>{data.allStrapiArticle.nodes.map((el) => 
+          <div>
+            <strong>{el.title}</strong>
+            <ul>{el.description}</ul> 
+            <p>{el.author.name}</p> 
+          </div> )}
+        </div>
+        : 'loading'
+      } 
+    </Layout>
+  );
+};
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const query = graphql`
+  query {
+    allStrapiArticle {
+      nodes {
+        id
+        description
+        author {
+          name
+        }
+        title
+      }
+    }
+    strapiHomepage {
+      hero {
+        title
+      }
+      seo {
+        metaTitle
+        metaDescription
+        shareImage {
+          localFile {
+            publicURL
+          }
+        }
+      }
+    }
+  }
+`;
 
-export default IndexPage
+export default IndexPage;
