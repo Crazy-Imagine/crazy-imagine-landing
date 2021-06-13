03c19d875a5a76d55c661dee373a0675786b8915
Betsy Moreno <morenoelba2002@gmail.com>
2021-06-13 16:04:47 -0400

    fix horizontal scroll

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,45 +1,52 @@
-import React from 'react';
+import React from "react"
 
-import { Grid } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core"
 
-import PostCard from "./PostCard";
-import blockImage from '../images/gatsby-icon.png';
+import PostCard from "./PostCard"
+import blockImage from "../images/gatsby-icon.png"
+
+const useStyes = makeStyles({
+  container: {
+    width: "100%",
+  },
+})
 
 const BlogPost = () => {
-  return(
-    <Grid container spacing={4} justify='center'>
+  const classes = useStyes()
+  return (
+    <Grid container spacing={4} justify="center" className={classes.container}>
       <Grid item>
-        <PostCard 
-          image={blockImage} 
-          author='Carlos Becerra XD' 
-          tags='Wordpress' 
-          title='Prueba' 
-          description='Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500'  
+        <PostCard
+          image={blockImage}
+          author="Carlos Becerra XD"
+          tags="Wordpress"
+          title="Prueba"
+          description="Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500"
         />
       </Grid>
       <Grid item>
-        <PostCard 
-          image={blockImage} 
-          author='Carlos Becerra XD' 
-          tags='Wordpress' 
-          title='Prueba' 
-          description='lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm'  
-          share='Share' 
-          footerNumber='264'
-          variant='principal'
+        <PostCard
+          image={blockImage}
+          author="Carlos Becerra XD"
+          tags="Wordpress"
+          title="Prueba"
+          description="lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm"
+          share="Share"
+          footerNumber="264"
+          variant="principal"
         />
       </Grid>
       <Grid item>
-        <PostCard 
-          image={blockImage} 
-          author='Carlos Becerra XD' 
-          tags='Wordpress' 
-          title='Prueba Prueba Prueba Prueba Prueba'
-          description='lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm' 
-        />  
+        <PostCard
+          image={blockImage}
+          author="Carlos Becerra XD"
+          tags="Wordpress"
+          title="Prueba Prueba Prueba Prueba Prueba"
+          description="lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm lorep ipsummmm"
+        />
       </Grid>
     </Grid>
   )
-};
+}
 
-export default BlogPost;
+export default BlogPost
