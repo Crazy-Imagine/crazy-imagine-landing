53b4808e97f486a2d7d7d1663a7cc4f482e740e3
Betsy Moreno <morenoelba2002@gmail.com>
2021-06-20 15:42:19 -0400

    fix: footer section align

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,95 +1,134 @@
-import React from 'react';
+import React from "react"
 
-import { Box, CardMedia, Grid, makeStyles, Typography } from "@material-ui/core";
-import RoomIcon from '@material-ui/icons/Room';
-import PhoneIcon from '@material-ui/icons/Phone';
-import EmailIcon from '@material-ui/icons/Email';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { Box, CardMedia, Grid, makeStyles, Typography } from "@material-ui/core"
+import RoomIcon from "@material-ui/icons/Room"
+import PhoneIcon from "@material-ui/icons/Phone"
+import EmailIcon from "@material-ui/icons/Email"
+import FacebookIcon from "@material-ui/icons/Facebook"
+import InstagramIcon from "@material-ui/icons/Instagram"
+import TwitterIcon from "@material-ui/icons/Twitter"
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
 
-import footerLogo from '../images/logo.jpeg';
+import footerLogo from "../images/logo.jpeg"
+import Section from "./Section"
 
 const useStyles = makeStyles({
   root: {
-    backgroundColor: '#1D1D1D',
-    color: 'white'
+    color: "white",
   },
   footerLogo: {
     width: 147,
-    height: 78
+    height: 78,
   },
   sectionTitle: {
     marginBottom: 14,
-    color: 'white',
+    color: "white",
     fontSize: 24,
-    fontWeight: 'bold',
-    lineHeight: '59px'
+    fontWeight: "bold",
+    lineHeight: "59px",
   },
   sectionLink: {
     marginBottom: 18,
     fontSize: 19,
-    lineHeight: '21px'
+    lineHeight: "21px",
   },
   footerIcons: {
     marginRight: 8,
-    marginTop: 16
+    marginTop: 16,
   },
   formatContactSection: {
-    '& $sectionLink': {
-      marginBottom: 8
+    "& $sectionLink": {
+      marginBottom: 8,
     },
-    '& $sectionTitle': {
-      lineHeight: 'inherit',
+    "& $sectionTitle": {
+      lineHeight: "inherit",
       marginBottom: 4,
-      marginTop: 13
-    } 
-  }
-});
+      marginTop: 13,
+    },
+  },
+})
 
 const Footer = () => {
-  const classes = useStyles();
-  return(
-   <Box className={classes.root} px={20} py={5}>
-    <Grid container spacing={1}>
-      <Grid item xs={6} sm={3}>
-        <CardMedia
-          image={footerLogo}
-          title='footerImage'
-          className={classes.footerLogo}
-        />
-        <Box className={classes.formatContactSection}>
-            <Typography variant='h3' className={classes.sectionTitle}>CONTACT</Typography>
-            <Typography className={classes.sectionLink}><RoomIcon /> Urb Ambroio Plaza #1 street</Typography>
-            <Typography className={classes.sectionLink}>House #2/123, San Cristobal, Venezuela.</Typography>
-            <Typography className={classes.sectionLink}><PhoneIcon /> +58 414 70079236</Typography>
-            <Typography className={classes.sectionLink}><EmailIcon / > contact@crazyimagine.com</Typography>
-        </Box>
-      </Grid>
-      <Grid item xs={6} sm={3}>
-        <Typography variant='h3' className={classes.sectionTitle}>ABOUT US</Typography>
-        <Typography className={classes.sectionLink}>Projects</Typography>
-        <Typography className={classes.sectionLink}>Out Team</Typography>
-        <Typography className={classes.sectionLink}>About Crazy Imagine</Typography>
-      </Grid>
-        <Grid item xs={6} sm={3}>
-          <Typography variant='h3' className={classes.sectionTitle}>COMPANY</Typography>
-          <Typography className={classes.sectionLink}>What else we povide</Typography>
-          <Typography className={classes.sectionLink}>Our Tecnologies</Typography>
-          <Typography className={classes.sectionLink}>Our Stats</Typography>
-          <Typography className={classes.sectionLink}>What our clients say</Typography>
+  const classes = useStyles()
+  return (
+    <Section backgroundColor="#1D1D1D" width="1060px">
+      <Box py={5} className={classes.root}>
+        <Grid container>
+          <Grid item xs={12} sm={5}>
+            <CardMedia
+              image={footerLogo}
+              title="footerImage"
+              className={classes.footerLogo}
+            />
+            <Box className={classes.formatContactSection}>
+              <Typography variant="h3" className={classes.sectionTitle}>
+                CONTACT
+              </Typography>
+              <Typography className={classes.sectionLink}>
+                <RoomIcon /> Urb Ambroio Plaza #1 street
+              </Typography>
+              <Typography className={classes.sectionLink}>
+                House #2/123, San Cristobal, Venezuela.
+              </Typography>
+              <Typography className={classes.sectionLink}>
+                <PhoneIcon /> +58 414 70079236
+              </Typography>
+              <Typography className={classes.sectionLink}>
+                <EmailIcon /> contact@crazyimagine.com
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item container xs={12} sm={7} spacing={1}>
+            <Grid item xs={4}>
+              <Typography variant="h3" className={classes.sectionTitle}>
+                ABOUT US
+              </Typography>
+              <Typography className={classes.sectionLink}>Projects</Typography>
+              <Typography className={classes.sectionLink}>Out Team</Typography>
+              <Typography className={classes.sectionLink}>
+                About Crazy Imagine
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h3" className={classes.sectionTitle}>
+                COMPANY
+              </Typography>
+              <Typography className={classes.sectionLink}>
+                What else we povide
+              </Typography>
+              <Typography className={classes.sectionLink}>
+                Our Tecnologies
+              </Typography>
+              <Typography className={classes.sectionLink}>Our Stats</Typography>
+              <Typography className={classes.sectionLink}>
+                What our clients say
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h3" className={classes.sectionTitle}>
+                GET STARTED
+              </Typography>
+              <Typography className={classes.sectionLink}>
+                Contact Us
+              </Typography>
+              <Typography className={classes.sectionLink}>
+                Our References
+              </Typography>
+              <Typography className={classes.sectionLink}>
+                Last Posts
+              </Typography>
+              <Typography>
+                <FacebookIcon className={classes.footerIcons} />{" "}
+                <InstagramIcon className={classes.footerIcons} />{" "}
+                <TwitterIcon className={classes.footerIcons} />{" "}
+                <LinkedInIcon className={classes.footerIcons} />
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
-      <Grid item xs={6} sm={3}>
-        <Typography variant='h3' className={classes.sectionTitle}>GET STARTED</Typography>
-        <Typography className={classes.sectionLink}>Contact Us</Typography>
-        <Typography className={classes.sectionLink}>Our References</Typography>
-        <Typography className={classes.sectionLink}>Last Posts</Typography>
-        <Typography ><FacebookIcon className={classes.footerIcons} /> <InstagramIcon className={classes.footerIcons} /> <TwitterIcon className={classes.footerIcons} /> <LinkedInIcon className={classes.footerIcons} /></Typography>
-      </Grid>
-    </Grid>
-   </Box>
-  ); 
-};
+      </Box>
+    </Section>
+  )
+}
 
-export default Footer;
+export default Footer
