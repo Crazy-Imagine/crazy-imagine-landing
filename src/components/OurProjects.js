5d011ff2a847bb6d8ea92fe867484051e028ae0c
Betsy Moreno <morenoelba2002@gmail.com>
2021-07-02 21:21:30 -0400

    feature: projects page

---
+++ src/components/OurProjects.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Box, Typography } from "@material-ui/core"
+
+const OurProjects = () => {
+  return (
+    <Box>
+      <Typography>OurProjects</Typography>
+    </Box>
+  )
+}
+
+export default OurProjects
---
+++ src/components/ProjectsServices.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Box, Typography } from "@material-ui/core"
+
+const ProjectsServices = () => {
+  return (
+    <Box>
+      <Typography>ProjectsServices</Typography>
+    </Box>
+  )
+}
+
+export default ProjectsServices
