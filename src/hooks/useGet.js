8c50c6ce839b961cd7654030d2a0222310ca7bad
DanielMora <danielalejandrolmoratorres@gmail.com>
2022-07-15 11:36:18 -0400

    enviroment variable

--- src/hooks/useGet.js
+++ src/hooks/useGet.js
@@ -4,15 +4,15 @@ export const useGet = (table, key, lang) => {
   const [contentReviews, setContentReviews] = useState([]);
   const getStrapi = async () => {
     //console.log("aqui", process.env.API_URL) //Is undefined
+    const domain = process.env.API_URL || "http://localhost:1337"
     if (lang === "es") {
       if (key === "false") {
-        const domain = process.env.API_URL || "http://localhost:1337"
         const url = `${domain}/${table}?_locale=es-VE`
         // || `http://3.91.249.33:1337/${table}?_locale=es-VE` 
         const resp = await fetch(url).then(response => response.json())
           .then(data => { setContentReviews(data) });
       } else {
-        const url = `http://3.91.249.33:1337/${table}?_locale=es-VE&_Key=${key}`
+        const url = `${domain}/${table}?_locale=es-VE&_Key=${key}`
         const resp = await fetch(url).then(response => response.json())
           .then(data => { setContentReviews(data) });
       }
