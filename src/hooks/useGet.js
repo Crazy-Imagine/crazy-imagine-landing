a026c713397bc188a3d590e2fdb64d7b1c686b53
DanielMora <danielalejandrolmoratorres@gmail.com>
2022-07-18 10:46:36 -0400

    commit unnecesary comments remove

--- src/hooks/useGet.js
+++ src/hooks/useGet.js
@@ -3,12 +3,10 @@ import { useState, useEffect } from "react"
 export const useGet = (table, key, lang) => {
   const [contentReviews, setContentReviews] = useState([]);
   const getStrapi = async () => {
-    //console.log("aqui", process.env.API_URL) //Is undefined
     const domain = process.env.API_URL || "http://localhost:1337"
     if (lang === "es") {
       if (key === "false") {
         const url = `${domain}/${table}?_locale=es-VE`
-        // || `http://3.91.249.33:1337/${table}?_locale=es-VE` 
         const resp = await fetch(url).then(response => response.json())
           .then(data => { setContentReviews(data) });
       } else {
